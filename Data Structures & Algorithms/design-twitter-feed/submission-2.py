7163d1785b2acc13995b3f84b0778ea80cf13706
Alisher Mamunov <mamunovalisher@gmail.com>
2026-04-08 23:39:16 -0500

    Add: design-twitter-feed - submission-2

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-2.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+import heapq
+
+class Twitter:
+
+    def __init__(self):
+        self.followees = defaultdict(set)
+        self.tweets = defaultdict(list)
+        self.count = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweets[userId].append((self.count, tweetId))
+        self.count += 1
+    
+    def getNewsFeed(self, userId: int) -> List[int]:
+        feed = []
+        for u in self.followees[userId] | {userId}:
+            for tweet in self.tweets[u]:
+                heapq.heappush(feed, tweet)
+                if len(feed) > 10:
+                    heapq.heappop(feed)
+        return [x[1] for x in sorted(feed, reverse=True)]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.followees[followerId].add(followeeId)
+
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.followees[followerId]:
+            self.followees[followerId].remove(followeeId)
+        
