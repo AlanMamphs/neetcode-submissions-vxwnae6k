2646a113e8f4ecd6046112ea96c3f7088b04d1dc
Alisher Mamunov <mamunovalisher@gmail.com>
2026-04-15 23:54:36 -0500

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        R = len(grid)
+        C = len(grid[0])
+        visited = set()
+        q = deque()
+        count = 0
+        for r in range(R):
+            for c in range(C):
+                v = grid[r][c]
+                if v == 1:
+                    count += 1
+                elif v == 2:
+                    q.append((r, c))
+                    visited.add((r, c))
+        minutes = 0
+        while count and q:
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                grid[r][c] = 2
+                for dr, dc in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+                    r1, c1 = r + dr, c + dc
+
+                    if r1 not in range(R) or c1 not in range(C) or (r1, c1) in visited or grid[r1][c1] == 0:
+                        continue
+                    q.append((r1, c1))
+                    visited.add((r1, c1))
+                    count -= 1
+
+            minutes += 1
+        return minutes if not count else -1
