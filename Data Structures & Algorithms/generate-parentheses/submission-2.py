8adaefbfa9e5627d437370980ddbbcc870e908f3
Alisher Mamunov <mamunovalisher@gmail.com>
2026-04-27 01:48:10 -0500

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+
+        def dfs(l , r, path):
+            if l == r == n:
+                res.append(path)
+                return
+            if l > n:
+                return 
+            
+            dfs(l + 1, r, path + '(')
+            if l > r:
+                dfs(l, r + 1, path + ')')
+            
+        dfs(0, 0, '')
+        return res
