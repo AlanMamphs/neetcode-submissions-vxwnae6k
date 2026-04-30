94a4ddb400ac6181f2ee7856f44fafa5dcbc2d27
Alisher Mamunov <mamunovalisher@gmail.com>
2026-04-30 02:56:34 -0500

    Add: combination-target-sum - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        nums.sort()
+        def dfs(i, total, curr):
+            if i == len(nums) or target <= total:
+                if total == target:
+                    res.append(curr.copy())
+                return 
+            curr.append(nums[i])
+            dfs(i, total + nums[i], curr)
+            curr.pop()
+            dfs(i + 1, total, curr)
+        
+        dfs(0, 0, [])
+        return res
+
