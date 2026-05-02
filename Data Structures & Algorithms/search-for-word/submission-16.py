a30ce3591be4cf98b5a5f558ed1bb074cdce4e75
Alisher Mamunov <mamunovalisher@gmail.com>
2026-05-02 15:15:25 -0500

    Add: search-for-word - submission-16

---
+++ Data Structures & Algorithms/search-for-word/submission-16.py
@@ -0,0 +1,28 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        R = len(board)
+        C = len(board[0])
+        
+        def dfs(r, c, i):
+            if r not in range(R) or c not in range(C):
+                return
+            
+            v = board[r][c]
+            if i + 1 == len(word) and v == word[i]:
+                return True
+
+            if v != word[i]:
+                return False
+            board[r][c] = '*'
+            for dr, dc in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                if dfs(r+dr, c+dc, i + 1):
+                    return True
+            board[r][c] = v
+            return False
+        for r in range(R):
+            for c in range(C):
+                if dfs(r, c, 0):
+                    return True
+
+        return False
+            
