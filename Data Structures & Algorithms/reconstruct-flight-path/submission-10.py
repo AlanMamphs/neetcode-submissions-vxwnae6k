ae2ae320952d1435fc8ece718f299c971415f58f
Alisher Mamunov <mamunovalisher@gmail.com>
2026-06-09 12:15:53 -0500

    Add: reconstruct-flight-path - submission-10

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-10.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        tickets.sort(reverse=True)
+        adj = defaultdict(list)
+        for s, d in tickets:
+            adj[s].append(d)
+        
+        res = []
+        q = ['JFK']
+
+        while q:
+            if adj[q[-1]]:
+                q.append(adj[q[-1]].pop())
+            else:
+                res.append(q.pop())
+        return res[::-1]
