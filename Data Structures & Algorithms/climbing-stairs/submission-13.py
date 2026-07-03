ce89a4a8d950716adc6afbe533fdb10b016332fe
Alisher Mamunov <mamunovalisher@gmail.com>
2026-07-03 17:38:10 -0500

    Add: climbing-stairs - submission-13

---
+++ Data Structures & Algorithms/climbing-stairs/submission-13.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        curr, prev = 1, 1
+
+        for i in range(n - 1):
+            curr, prev = curr + prev, curr
+        
+        return curr
