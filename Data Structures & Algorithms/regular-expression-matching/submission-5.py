33552c6d0f80b8d744a8f4e518ab3fbc96151cda
Alisher Mamunov <mamunovalisher@gmail.com>
2026-07-16 02:01:03 -0500

    Add: regular-expression-matching - submission-5

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-5.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = dict()
+        def dfs(i, j):
+            if (i, j) in dp:
+                return dp[(i,j)]
+            
+            if i == len(s) and j >= len(p):
+                return True
+            
+            if j >= len(p):
+                return False
+
+            match = i < len(s) and (s[i] == p[j] or p[j] == '.')
+            res = False
+            if j + 1 < len(p) and p[j+1] == '*':
+                res = dfs(i, j + 2) or (match and dfs(i+1, j))
+            elif match:
+                res = dfs(i+1, j+1)
+            dp[(i, j)] = res
+            return res
+        return dfs(0,0)
+
+
+
+
+
+
+
