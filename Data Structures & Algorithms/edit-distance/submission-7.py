34cc281498ed5b292c98c63162c9013f081a3183
Alisher Mamunov <mamunovalisher@gmail.com>
2026-07-30 06:24:01 -0500

    Add: edit-distance - submission-7

---
+++ Data Structures & Algorithms/edit-distance/submission-7.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        R = len(word1)
+        C = len(word2)
+        if R < C:
+            R, C, word1, word2 = C, R, word2, word1
+        dp = list(range(C, -1, -1))
+        ndp = [0] * (C + 1)
+        for r in range(R - 1, -1, -1):
+            ndp[-1] = R - r
+            for c in range(C -1, -1, -1):
+                if word1[r] == word2[c]:
+                    ndp[c] = dp[c + 1]
+                else:
+                    delete = dp[c]
+                    insert = ndp[c + 1]
+                    replace = dp[c + 1]
+
+                    ndp[c] = 1 + min(delete, insert, replace)
+            ndp, dp = dp, ndp
+        
+        return dp[0]
