ae6dbf984e4ca364d7ee0374210ca6a11b1db6af
Alisher Mamunov <mamunovalisher@gmail.com>
2026-08-16 22:55:16 -0500

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.py
@@ -0,0 +1,18 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x: x.start)
+        last = float('-inf')
+        for interval in intervals:
+            if interval.start >= last:
+                last = interval.end
+            else:
+                return False
+        return True
