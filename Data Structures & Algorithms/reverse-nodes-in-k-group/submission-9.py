4f0dc06e30f98d36188e2fdddd7d78fcfa41fa32
Alisher Mamunov <mamunovalisher@gmail.com>
2026-09-20 19:01:04 -0500

    Add: reverse-nodes-in-k-group - submission-9

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-9.py
@@ -0,0 +1,50 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverse(self, l):
+        curr = l
+        prev = None
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
+        return prev
+    def kth(self, l, k):
+        while k and l:
+            l = l.next
+            k -= 1
+        return l
+
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        root = ListNode(0)
+        curr = root
+        next_kth = head
+        
+        while next_kth:
+            end = self.kth(next_kth, k - 1)
+            if not end:
+                break
+            tmp = end.next
+            end.next = None
+            curr.next = self.reverse(next_kth)
+            curr = next_kth
+            next_kth = tmp
+        
+        curr.next = next_kth
+
+        # end = self.kth(next_kth, k - 1)
+        # tmp = end.next
+        # end.next = None
+        # curr.next = self.reverse(next_kth)
+        # curr = end
+        # next_kth = tmp
+
+        return root.next
+        #     # if not end:
+        #     #     break
+        #     next_kth, end.next, curr.next, curr = end.next, None, self.reverse(next_kth), next_kth
+        # curr.next = next_kth
+    
+        # return root.next
