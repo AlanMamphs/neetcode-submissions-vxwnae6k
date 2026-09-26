6b40528d022226641b4afd645a13e18f82ec40f3
Alisher Mamunov <mamunovalisher@gmail.com>
2026-09-26 13:52:34 -0500

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.py
@@ -0,0 +1,18 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.store = OrderedDict()
+        self.capacity = capacity
+        
+    def get(self, key: int) -> int:
+        if key not in self.store:
+            return -1
+        self.store.move_to_end(key)
+        return self.store[key]
+
+    def put(self, key: int, value: int) -> None:
+        self.store[key] = value
+        self.store.move_to_end(key)
+        if len(self.store) > self.capacity:
+            self.store.popitem(last=False)
+        
